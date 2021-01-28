cf6071c0643328a7381dcbe863a41aa0611c0dc7
nightted <h5904098@gmail.com>
2021-01-28 16:39:03 +0800

    add get_search_result_by_resturant function.

--- travelproject/bot/google_search_and_show.py
+++ travelproject/bot/google_search_and_show.py
@@ -1,11 +1,21 @@
 from googlesearch import search
 from webpreview import web_preview
 
-query = "文章牛肉湯"
-urls = []
+def get_search_result_by_resturant(resturant_name):
 
-for url in search(query, stop=5, pause=2.0):
-	urls.append(url)
+    result = []
+    for result_url in search(resturant_name, stop=5, pause=2.0):
+        if 'facebook' not in result_url:
+            res = web_preview(result_url)
+            preview_pic_url = res[2]
+            break
 
-for url in urls:
-    print(web_preview(url))
+    return result_url , preview_pic_url
+
+
+if __name__ == '__main__':
+
+    query = '春川煎餅'
+    result = get_search_result_by_resturant(query)
+
+    print(result)
