98f52cc2b06a4ef9bb5bc8250c10e0c1dee90908
nightted <h5904098@gmail.com>
2021-01-28 16:36:19 +0800

    No change.

--- travelproject/bot/constants.py
+++ travelproject/bot/constants.py
@@ -1,11 +1,15 @@
 # GLOBAL　PARAMETERS
 
+import configparser
+from bot.tools import read_key
+
+# geometry property of taiwan
 lng_1 = 102.516520*1000 # 1 longitude to meters
 lat_1 = 110.740000*1000 # 1 latitude to meters
-
 Admin_area_range_lng = [120.03786531340755 , 122.00991123709818] # range of lng in taiwan
 Admin_area_range_lat = [21.871068186336803 , 25.30245194059663] # range of lng in taiwan
 
+# city property
 center_of_city = {
     "Tainan" : {'location' : {'lat': 22.9913113, 'lng': 120.198012} ,
                 'city_en_to_cn' : '台南' ,
@@ -21,9 +25,34 @@ center_of_city = {
                'popular_food' : ['糕渣' , '鴨賞'] },
 }
 
+# URL of booking.com
+BOOKING_URL = 'https://www.booking.com'
+BASE_BOOKING_URL = BOOKING_URL + '/searchresults.zh-tw.html'
+
+# secret key path
 KEY_PATH = 'C:/Users/h5904/PycharmProjects/TravelProject/travelproject/docs/API_KEY.txt' # TODO : set as enviroment variables
 SECRET_KEY_PATH = 'C:/Users/h5904/PycharmProjects/TravelProject/travelproject/docs/SECRET_KEY.txt'
 ACCESS_TOKEN_PATH = 'C:/Users/h5904/PycharmProjects/TravelProject/travelproject/docs/keys/CHANNEL_ACCESS_TOKEN.txt'
 SECRET_PATH = 'C:/Users/h5904/PycharmProjects/TravelProject/travelproject/docs/keys/CHANNEL_SECRET.txt'
-BOOKING_URL = 'https://www.booking.com'
-BASE_BOOKING_URL = BOOKING_URL + '/searchresults.zh-tw.html'
+
+
+
+if __name__ == '__main__':
+
+    config = configparser.ConfigParser()
+    '''config['path'] = {}
+
+    path = config['path']
+    path['GOOGLE_MAP_API_KEY'] = read_key(KEY_PATH)
+    path['DJANGO_SECRET_KEY'] = read_key(SECRET_KEY_PATH)
+    path['CHANNEL_ACCESS_TOKEN'] = read_key(ACCESS_TOKEN_PATH)
+    path['CHANNEL_SECRET'] = read_key(SECRET_PATH)
+
+    with open('config.ini', 'w') as configfile:
+        config.write(configfile)'''
+
+    config.read('config.ini')
+    print(config.sections())
+
+
+
