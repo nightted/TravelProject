9c403d295645b3f789636422d18b73e62274905a
nightted <h5904098@gmail.com>
2021-01-29 22:57:24 +0800

    minor change.

--- travelproject/bot/google_search_and_show.py
+++ travelproject/bot/google_search_and_show.py
@@ -1,21 +1,23 @@
 from googlesearch import search
 from webpreview import web_preview
 
-def get_search_result_by_resturant(resturant_name):
+def get_search_result_by_resturant(resturant_obj):
 
-    result = []
-    for result_url in search(resturant_name, stop=5, pause=2.0):
+    result_dict = {}
+    for result_url in search(resturant_obj.name, stop=5, pause=2.0):
         if 'facebook' not in result_url:
             res = web_preview(result_url)
             preview_pic_url = res[2]
+            result_dict.update(
+                                {'result_url' : result_url,
+                                'preview_pic_url' : preview_pic_url ,
+                                'name': resturant_obj.name ,
+                                'rating': resturant_obj.rating
+                                }
+                              )
             break
 
-    return result_url , preview_pic_url
+    print("DEBUG : " , result_dict)
 
+    return result_dict
 
-if __name__ == '__main__':
-
-    query = '春川煎餅'
-    result = get_search_result_by_resturant(query)
-
-    print(result)
