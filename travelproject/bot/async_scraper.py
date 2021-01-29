5abd00fa26d6eff510a4d4cdea16bdd7dc6432aa
nightted <h5904098@gmail.com>
2021-01-29 22:56:54 +0800

    add async for google search.

--- travelproject/bot/async_scraper.py
+++ travelproject/bot/async_scraper.py
@@ -1,5 +1,6 @@
 import asyncio
 from bot.booking_scraper import get_hotel_information
+from bot.google_search_and_show import *
 from bot.tools import  get_date_string
 
 from functools import partial
@@ -31,6 +32,33 @@ def async_main_construct_step_by_hotel(hotels):
 
 
 
+
+# TODO : async search
+# Async for get hotel information
+async def async_get_search_result(resturant):
+
+    # from : https://stackoverflow.com/questions/53368203/passing-args-kwargs-to-run-in-executor
+    res = await loop.run_in_executor(None , partial(get_search_result_by_resturant, resturant ))
+    return res
+
+def tasks_generator_for_get_search_result_by_resturant(resturants): # 定義一個中間會被中斷的協程
+
+    tasks = []
+    for resturant in resturants :
+        tasks.append(loop.create_task(async_get_search_result(resturant)))
+    return tasks
+
+def async_get_search_result_by_resturant(resturants):
+
+    tasks = tasks_generator_for_get_search_result_by_resturant(resturants)
+    res  , _ = loop.run_until_complete(asyncio.wait(tasks)) # https://stackoverflow.com/questions/44048536/python3-get-result-from-async-method
+    res_dict = [ele.result() for ele in res]
+
+    return res_dict
+
+
+
+
 # Async for get hotel information
 async def async_get_hotel_information( **kwargs ):
 
@@ -41,7 +69,6 @@ async def async_get_hotel_information( **kwargs ):
 def tasks_generator_for_get_hotel_information_by_date( dates , **kwargs ): # 定義一個中間會被中斷的協程
 
     tasks = []
-
     for date in dates :
         tasks.append(loop.create_task(async_get_hotel_information(  **kwargs , date = date )))
     return tasks
