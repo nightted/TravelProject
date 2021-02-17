f2aaca9056733d8de2d2c13b39494685d6c63ab3
tedchang <h5904098@gmail.com>
2021-02-17 23:01:27 +0800

    dict add position xy.

--- travelproject/bot/google_search_and_show.py
+++ travelproject/bot/google_search_and_show.py
@@ -6,13 +6,15 @@ def get_search_result_by_resturant(resturant_obj):
     result_dict = {}
     for result_url in search(resturant_obj.name, stop=5, pause=2.0):
         if 'facebook' not in result_url:
+            #print('DEBUG in web_preview:' , result_url)
             res = web_preview(result_url)
             preview_pic_url = res[2]
             result_dict.update(
                                 {'result_url' : result_url,
-                                'preview_pic_url' : preview_pic_url ,
-                                'name': resturant_obj.name ,
-                                'rating': resturant_obj.rating
+                                 'preview_pic_url' : preview_pic_url ,
+                                 'name': resturant_obj.name ,
+                                 'rating': resturant_obj.rating,
+                                 'position_xy': [resturant_obj.lng , resturant_obj.lat]
                                 }
                               )
             break
