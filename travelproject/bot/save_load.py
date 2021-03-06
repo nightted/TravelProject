97eff923be346fd157eb38fabfe1e3522e7f6712
tedchang <h5904098@gmail.com>
2021-03-06 14:42:44 +0800

    minor change.

--- travelproject/bot/save_load.py
+++ travelproject/bot/save_load.py
@@ -12,15 +12,4 @@ def load_pkl(path):
 
 if __name__  == '__main__' :
 
-    abs_file_paths = {}
-    Tainan_all_objects = {}
-    cur_path = 'C:/Users/h5904/PycharmProjects/Travel_Recommend_project/Travel_Recommend/city data/Tainan/dict data/Tainan_all_objects_dict'
-    Tainan_all_objects = load_pkl(cur_path)
-
-    gruels_dicts = Tainan_all_objects['gruel']
-    for gruel_dict in gruels_dicts:
-       obj = Resturant.create_obj_by_dict(gruel_dict)
-       obj.save()
-
-
-
+    pass
