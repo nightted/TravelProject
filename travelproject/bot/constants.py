d5f8f22bfb8b1d6229323b70ca043a55ea432aca
nightted <h5904098@gmail.com>
2021-03-09 21:50:46 +0800

    add scrape condition into global constant - 'center_of_city'.

--- travelproject/bot/constants.py
+++ travelproject/bot/constants.py
@@ -15,24 +15,39 @@ lat_1 = 110.740000*1000 # 1 latitude to meters
 Admin_area_range_lng = [120.03786531340755 , 122.00991123709818] # range of lng in taiwan
 Admin_area_range_lat = [21.871068186336803 , 25.30245194059663] # range of lng in taiwan
 
+# basic scraper property
+basic_scrape = {
+                '飯店' : {'radius' : 500 , 'ranging' : 4 , 'place_type' : 'hotel' , 'place_sub_type' : 'hotel'} , # TODO (備忘) : need to construct hotel first due to manytomany field !!!!!
+                '餐廳' : {'radius' : 500 , 'ranging' : 4 , 'place_type' : 'resturant' , 'place_sub_type' : 'resturant'} ,
+                '便利商店' : {'radius' : 500 , 'ranging' : 4 , 'place_type' : 'resturant' , 'place_sub_type' : 'con'} ,
+                '停車場' : {'radius' : 1000 , 'ranging' : 3 , 'place_type' : 'station' , 'place_sub_type' : 'parking'} ,
+                '夜市' : {'radius' : 2000 , 'ranging' : 2 , 'place_type' : 'sightseeing' , 'place_sub_type' : 'nightmarket'} ,
+                '觀光景點' : {'radius' : 3000 , 'ranging' : 4 , 'place_type' : 'sightseeing' , 'place_sub_type' : 'sightseeing'} ,
+                '火車站' : {'radius' : 5000 , 'ranging' : 1 , 'place_type' : 'station' , 'place_sub_type' : 'station'} ,
+                }
+
 # city property
 center_of_city = {
     "Tainan" : {'location' : {'lat': 22.9913113, 'lng': 120.198012} ,
                 'city_en_to_cn' : '台南' ,
                 'popular_food' : ['牛肉湯','肉燥飯','鹹粥','鱔魚意麵'] ,
-                'city_range' : {'lat' : [22.887709 ,23.413328] , 'lng' : [120.033587 ,120.656311]}} ,
+                'city_range' : {'lat' : [22.887709 ,23.413328] , 'lng' : [120.033587 ,120.656311]},
+                'scrape_inform' : basic_scrape} ,
     "Hsinchu" : {'location' : {'lat': 24.8015877, 'lng': 120.9715883} ,
                  'city_en_to_cn' : '新竹' ,
                  'popular_food' : ['米粉' , '貢丸'] ,
-                 'city_range' : {'lat' : [24.428411 ,24.946137] , 'lng' : [120.878891 ,121.412232]}},
+                 'city_range' : {'lat' : [24.428411 ,24.946137] , 'lng' : [120.878891 ,121.412232]},
+                 'scrape_inform' : basic_scrape},
     "Hualien" : {'location' : {'lat': 23.9927385, 'lng': 121.6013407} ,
                  'city_en_to_cn' : '花蓮' ,
                  'popular_food' : ['扁食'] ,
-                 'city_range' : {'lat' : [23.099277 ,24.370550] , 'lng' : [120.989121 ,121.647408]}},
+                 'city_range' : {'lat' : [23.099277 ,24.370550] , 'lng' : [120.989121 ,121.647408]},
+                 'scrape_inform' : basic_scrape},
     "Yilan" : {'location' : {'lat': 24.6783841, 'lng': 121.7745634} ,
                'city_en_to_cn' : '宜蘭' ,
                'popular_food' : ['糕渣' , '鴨賞'] ,
-               'city_range' : {'lat' : [24.312153 ,24.866411] , 'lng' : [121.316842 ,121.885963]}},
+               'city_range' : {'lat' : [24.312153 ,24.866411] , 'lng' : [121.316842 ,121.885963]},
+               'scrape_inform' : basic_scrape},
 }
 
 # URL of booking.com
